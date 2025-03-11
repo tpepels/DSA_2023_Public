3b9be2e511b8de05aef733c93f7935072835b0ca
Tom <tpepels@gmail.com>
2025-03-11 15:35:11 +0100

    Add additional test cases for SkippingProblemExample and print DP array

--- module_6/SkippingProblemExample.java
+++ module_6/SkippingProblemExample.java
@@ -16,6 +16,7 @@ public class SkippingProblemExample {
             dp[i] = steps[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
         // Print the array with the inbuilt function
+        System.out.print("DP Array: ");
         System.out.println(Arrays.toString(dp));
 
         return dp[steps.length - 1];
@@ -23,9 +24,24 @@ public class SkippingProblemExample {
 
     public static void main(String[] args) {
         int[] steps = { 1, 2, 3, 1, 5, 1, 2, 1 };
+
+        int[] steps_2 = { 1, 2 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_2));
+        int[] steps_3 = { 1, 2, 3 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_3));
+        int[] steps_4 = { 1, 2, 3, 1 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_4));
+        int[] steps_5 = { 1, 2, 3, 1, 5 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_5));
+        int[] steps_6 = { 1, 2, 3, 1, 5, 1 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_6));
+        int[] steps_7 = { 1, 2, 3, 1, 5, 1, 2 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_7));
+        System.out.println("Final solution:");
         System.out.println("Minimum Total Cost: " + minTotalCost(steps));
 
-        int[] steps_2 = { 6, 2, 3, 8, 1, 2, 2, 1 };
-        System.out.println("Minimum Total Cost: " + minTotalCost(steps_2));
+        System.out.println("");
+        int[] steps_two = { 6, 2, 3, 8, 1, 2, 2, 1 };
+        System.out.println("Minimum Total Cost: " + minTotalCost(steps_two));
     }
 }
